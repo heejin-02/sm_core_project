7256622ab4ff4241a0c405ff241e99f459b41e8f
Kimhyerim <ax1021@naver.com>
2025-05-16 10:57:20 +0900

    [ADD] Discuss_roomVO 추가

--- Core_project/src/main/java/com/core/model/Discuss_roomVO.java
+++ Core_project/src/main/java/com/core/model/Discuss_roomVO.java
@@ -9,5 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Discuss_roomVO {
-
+	private int DROOM_NO;
+	private String DROOM_CATEGORY;
+	private String DROOM_TITLE;
+	private String DROOM_INFO;
+	private String ID;
+	private int DROOM_LIMIT;
 }

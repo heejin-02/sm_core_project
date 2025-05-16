0fc2b74ae024b77f4db3e7e00d78fc9222460447
heejin-02 <lumosmaxima_@naver.com>
2025-05-16 10:42:23 +0900

    [ADD] ChattingVO 작성

--- Core_project/src/main/java/com/core/model/ChattingVO.java
+++ Core_project/src/main/java/com/core/model/ChattingVO.java
@@ -1,5 +1,7 @@
 package com.core.model;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChattingVO {
 
+	private int CHAT_NO;
+	private int DROOM_NO;
+	private String CHATTER;
+	private String CHAT_CONTENT;
+	private String CHAT_EMOTICON;
+	private String CHAT_FILE;
+	private LocalDateTime CREATED_AT;
+	private String BLOCK_YN;
+	
 	
 }

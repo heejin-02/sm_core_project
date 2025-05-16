d47bd304c7c374a7abf62b74e7ae2f2728fe1090
heejin-02 <lumosmaxima_@naver.com>
2025-05-16 10:16:47 +0900

    기본 프로젝트 생성

---
+++ Core_project/src/main/java/com/core/controller/coreController.java
@@ -0,0 +1,53 @@
+package com.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class coreController {
+
+	//회원가입 메서드(join)
+	
+	
+	
+	
+	
+	//로그인 메서드(login)
+	
+	
+	
+	
+	
+	//정책 제안 올리기 메서드(proposal_post)
+	
+	
+	
+	
+	
+	//정책 제안 목록 띄우기 메서드(proposal_list)
+	
+	
+	
+	
+	
+	//유사도 목록 띄우기 메서드(similar_search)
+	
+	
+	
+	
+	
+	//토론방 목록 띄우기 메서드(discuss_list)
+	
+	
+	
+	
+	
+	//토론방 생성하기 메서드(discuss_post)
+	
+	
+	
+	
+	
+	
+	//토론방 안 채팅 띄우기 메서드(discuss_room)
+	// (제일 나중에하기)
+}

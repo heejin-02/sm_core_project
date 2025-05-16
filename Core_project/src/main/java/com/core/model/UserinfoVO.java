f55c16a15d13ffad89433d9cb0549dc848cfb6f3
ckaudgns89 <ckaudgns89@naver.com>
2025-05-16 11:00:57 +0900

    [ADD]UserinfoVO_작성

--- Core_project/src/main/java/com/core/model/UserinfoVO.java
+++ Core_project/src/main/java/com/core/model/UserinfoVO.java
@@ -1,5 +1,7 @@
 package com.core.model;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserinfoVO {
-
+public class UserinfoVO { //유저정보
+	
+	private String ID; //아이디
+	private String PW; // 비밀번호
+ 	private String NICK; // 닉네임
+	private String REGION; // 지역
+	private String ID_CARD; // 신분증 사진
+	private String IS_APPROVED; // 인증 여부
+	private LocalDateTime JOINED_AT; // 가입일자
 }
